81f96a2798fbc666ef6f51e38c92b90c614d9c71
tamagosan2424 <51788189+tamagosan2424@users.noreply.github.com>
2026-05-24 23:01:49 +0900

    Laravel Breeze導入

---
+++ src/database/seeders/initCafesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB; //追加
+use Illuminate\Support\Str; //追加
+use Illuminate\Support\Facades\Hash; //追加
+use Illuminate\Database\Seeder;
+
+class initCafesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
