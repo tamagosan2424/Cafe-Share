061ad78dc381564a249030ce6156d947802635e4
tamagosan2424 <51788189+tamagosan2424@users.noreply.github.com>
2026-05-26 22:53:03 +0900

    seederの導入

--- src/database/seeders/initCafesSeeder.php
+++ src/database/seeders/initCafesSeeder.php
@@ -13,8 +13,12 @@ class initCafesSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email'     => Str::random(10).'@gmail.com',
+            'password'  => Hash::make('password'),
+        ]);
     }
 }
