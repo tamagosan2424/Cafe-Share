06df384f7ce981e3ea6b905964b66f7efe2e7262
tamagosan2424 <51788189+tamagosan2424@users.noreply.github.com>
2026-06-03 05:14:55 +0900

    コントローラーファイルの修正

--- src/app/Http/Controllers/CafeController.php
+++ src/app/Http/Controllers/CafeController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\user;
 
 class CafeController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all(); //データを取得
+        return view('cafe.index',['users'=>$users]);
+    }
 }
