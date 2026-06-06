7b61538491268ff57b4ca5ef1c33f840e924ca78
tamagosan2424 <51788189+tamagosan2424@users.noreply.github.com>
2026-06-07 00:11:51 +0900

    Vue版Indexを作成しBladeファイルを削除

---
+++ src/resources/js/Pages/Cafe/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    users: Array,
+});
+</script>
+
+<template>
+    <Head title="カフェ一覧" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                カフェ一覧
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <ul>
+                            <li v-for="user in users" :key="user.id">
+                                {{ user.name }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
